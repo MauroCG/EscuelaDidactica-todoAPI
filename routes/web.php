3ba570099dec1992b0db8354253a9305aec0d4e9
mauro57.mauricio <mauro57.mauricio@gmail.com>
2025-04-16 20:12:30 +0000

    Moved api routes to api.php and redefined the RouteServiceProvider file

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::group(['middleware' => 'api', 'prefix' => 'api'], function () {
-    Route::get('/users', [\App\Http\Controllers\UserController::class, 'index']);
-    Route::post('/users', [\App\Http\Controllers\UserController::class, 'store']);
-
-    Route::get('/tasks', [\App\Http\Controllers\TaskController::class, 'index']);
-    Route::post('/tasks', [\App\Http\Controllers\TaskController::class, 'store']);
-    Route::put('/tasks/{task}', [\App\Http\Controllers\TaskController::class, 'update']); // Assuming Route Model Binding
-    Route::delete('/tasks/{task}', [\App\Http\Controllers\TaskController::class, 'destroy']); // Assuming Route Model Binding
 });
